b8f20347d6044966a87566eb3dbadae359b1fb78
goncalopp <goncalopp@will.(none)>
2009-09-10 21:02:29 +0100

    point3d: added vectorTo() and distanceTo(); factorized code in rotateOver()

--- point3d_unit.pas
+++ point3d_unit.pas
@@ -14,10 +14,13 @@ type
     { private declarations }
   public
     x,y,z: double;
+    coordinates: array[0..2] of Pdouble;
     constructor create(x_,y_,z_: double);
     procedure moveto(x_, y_, z_: double);
     procedure translate(x_, y_, z_: double);
     procedure rotateOver(rotationaxis: char; point: Point3d; angle: double);
+    function vectorTo(point: Point3d): Point3d;
+    function distanceTo(point: Point3d): double;
   end;
 
 implementation
@@ -44,6 +47,9 @@ constructor Point3d.create(x_,y_,z_: double);
 	x:=x_;
 	y:=y_;
 	z:=z_;
+    coordinates[0]:=@x;
+    coordinates[1]:=@y;
+	coordinates[2]:=@z;
 	end;
 
 procedure Point3d.moveto(x_, y_, z_: double);
@@ -61,35 +67,42 @@ procedure Point3d.translate(x_, y_, z_: double);
 	end;
 
 procedure Point3d.rotateOver(rotationaxis: char; point: Point3d; angle: double);
-    var distance, current_angle: double;
+    var distance, current_angle: double; ce, c0, c1: integer;
  	begin
     if rotationaxis='x' then
-        begin
+    	begin
+     	ce:=0;          //excluded coordinate
+        c0:=1;          //first coordinate to process
+        c1:=2;          //second coordinate to process
+        end;
         
-        distance:= sqrt( (y-point.y)*(y-point.y) + (z-point.z)*(x-point.z) );
-		current_angle:=anglefromcoordinates(y-point.y, z-point.z);
-        x:=x;
-        y:=point.y+distance*cos(current_angle+angle);
-        z:=point.z+distance*sin(current_angle+angle);
-        end;
     if rotationaxis='y' then
-        begin
-		distance:= sqrt( (x-point.x)*(x-point.x) + (z-point.z)*(x-point.z) );
-		current_angle:=anglefromcoordinates(x-point.x, z-point.z);
-        x:=point.x+distance*cos(current_angle+angle);
-        y:=y;
-        z:=point.z+distance*sin(current_angle+angle);
-        end;
-	if rotationaxis='z' then
-        begin
-		distance:= sqrt( (x-point.x)*(x-point.x) + (y-point.y)*(y-point.y) );
-		current_angle:=anglefromcoordinates(x-point.x, y-point.y);
-        x:=point.x+distance*cos(current_angle+angle);
-        y:=point.y+distance*sin(current_angle+angle);
-        z:=z;
-        end;
+        begin c0:=0; ce:=1; c1:=2; end;
+        
+    if rotationaxis='z' then
+     	begin c0:=0; c1:=1; ce:=2; end;
+        
+    point.coordinates[ce]^:=coordinates[ce]^; 	//project the point into the plane of rotation...
+    distance:= distanceTo(point);               //...so we can calculate the distance in the plane
+    
+	current_angle:=anglefromcoordinates(        //calculate current angle in trig circle
+ 			coordinates[c0]^-point.coordinates[c0]^,
+    		coordinates[c1]^-point.coordinates[c1]^);
+
+    coordinates[c0]^:=point.coordinates[c0]^+distance*cos(current_angle+angle);
+    coordinates[c1]^:=point.coordinates[c1]^+distance*sin(current_angle+angle);
 	end;
 
+function Point3d.vectorTo(point: Point3d): Point3d;
+    begin
+    result:=Point3d.create(point.x-x, point.y-y, point.z-z);
+    end;
+    
+function Point3d.distanceTo(point: Point3d): double;
+    begin
+    result:=sqrt( (x-point.x)*(x-point.x) + (y-point.y)*(y-point.y) + (z-point.z)*(z-point.z));
+    end;
+    
 end.
 
 
