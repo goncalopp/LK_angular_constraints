08011afbc69da8d62f7ebb31a039a3f6a4a45e3d
goncalopp <goncalopp@will.(none)>
2010-05-26 08:40:49 +0100

    change in region_unit: made create() assume the bounds are ascendingly ordered -  no objects created

--- region_unit.pas
+++ region_unit.pas
@@ -21,14 +21,8 @@ implementation
 constructor Region.create(bound1, bound2: PointND);
 var i:integer;
 	begin
-	bounds[0]:= PointND.create(bound1);
-    bounds[1]:= PointND.create(bound2);
-    for i:=0 to length(bounds[0].c) do
-    	bounds[0].c[i]:=min(bound1.c[i], bound2.c[i]);
-    for i:=0 to length(bounds[1].c) do
-    	bounds[1].c[i]:=max(bound1.c[i], bound2.c[i]);
-	bound1.Destroy();
-    bound2.Destroy();
+	bounds[0]:= bound1;
+    bounds[1]:= bound2;
  	end;
 
 function Region.inside(point: PointND): boolean;
