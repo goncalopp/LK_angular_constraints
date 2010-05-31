c77b08a21d170681be41d0e6d28c1b0ab958a5eb
goncalopp <goncalopp@will.(none)>
2010-05-31 20:31:47 +0100

    added quicksort_unit

--- project1.lpr
+++ project1.lpr
@@ -10,7 +10,7 @@ uses
   Forms
   { you can add units after this }, Unit1, domain_unit, region_unit,
   atom_unit, sineWave_unit, linkedList_unit,
-  pointnd_unit, sinewaveinregion_unit, atomsine_calc_unit;
+  pointnd_unit, sinewaveinregion_unit, atomsine_calc_unit, quicksort_unit;
 
 begin
   Application.Initialize;
---
+++ quicksort_unit.pas
@@ -0,0 +1,68 @@
+unit quicksort_unit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+  TYPE
+  QSarray = ARRAY OF TObject;
+  QSPointer = ^TObject;
+  QSMapping = double;
+  QSFunction = Function(obj: TObject): QSMapping;
+
+implementation
+
+
+procedure swap(a,b: QSPointer);
+var t: TObject;
+	BEGIN
+    t :=  a^;
+    a^ := b^;
+    b^ := t
+    END;
+
+function Split(arr: QSarray; f: QSfunction; start, stop: integer): integer;
+var left, right: integer;
+    pivot: QSMapping;
+    BEGIN
+	pivot := f(arr[start]);
+    left := start + 1;
+    right := stop;
+    WHILE left <= right DO
+    	BEGIN
+    	WHILE (left <= stop) AND (f(arr[left]) < pivot) DO
+        	left := left + 1;
+        WHILE (right > start) AND (f(arr[right]) >= pivot) DO
+        	right := right - 1;
+        IF left < right THEN
+        	swap(@arr[left], @arr[right]);
+        END;
+
+    swap(@arr[start], @arr[right]);
+    result := right
+    END;
+
+
+PROCEDURE QuicksortRecur(arr: QSarray; f: QSfunction; start, stop: integer);
+VAR
+splitpt: integer;
+    BEGIN
+    IF start < stop THEN
+    	BEGIN
+        splitpt := Split(arr, f, start, stop);
+        QuicksortRecur(arr, f, start, splitpt-1);
+        QuicksortRecur(arr, f, splitpt+1, stop);
+        END
+    END;
+
+PROCEDURE Quicksort(arr: QSArray; f: QSFunction; size: Integer);
+	BEGIN
+	QuicksortRecur(arr, f, 0, size-1)
+    END;
+END.
+
+end.
+
