73190da21b7cb43babb7259a58887aa4bcb39f5c
goncalopp <goncalopp@will.(none)>
2010-07-27 23:40:18 +0100

    added partial validregions() function - not complete/correct

--- atomsine_calc_unit.pas
+++ atomsine_calc_unit.pas
@@ -142,6 +142,77 @@ function otherIntersectionSine(i: TIntersection; s: sinewave): sinewave;
     	result:=i.sine1;
     end;
 
+function validRegions(): LinkedList;
+var currentsirs: array [0..1] of LinkedNode; // ..of SinewaveInRegion;
+	valid_beggining, valid_ending, valid_current_region: boolean;
+    current_valid_region, current_region_intersection: TRegion;
+	intersection: PointND;
+    done_sir, i: integer;
+    intersectionwave: sinewave;
+	begin
+    for i:=0 to 1 do
+		begin
+    	sirs[i].rewind();
+        currentsirs[i]:= sirs[i].advance();
+        end;
+
+    result:=LinkedList.create();
+    current_region_intersection:= TRegion.create(  SineWaveInRegion(currentsirs[0].element).region.bounds[0]  ,         nil        );
+    current_valid_region:=nil;
+
+    while ((currentsirs[0]<> nil) and (currentsirs[1]<>nil)) do
+    	begin
+        done_sir:=0;
+        if ((currentsirs[0]=nil) or (SineWaveInRegion(currentsirs[0].element).region.bounds[1].c[0] > SineWaveInRegion(currentsirs[1].element).region.bounds[1].c[0])) then
+        	done_sir:= 1;
+        current_region_intersection.bounds[1]:=SineWaveInRegion(currentsirs[done_sir].element).region.bounds[1];
+        valid_beggining := SineWaveInRegion(currentsirs[0].element).sine.valueat(current_region_intersection.bounds[0].c[0]) < SineWaveInRegion(currentsirs[1].element).sine.valueat(current_region_intersection.bounds[0].c[0]);
+        valid_ending    := SineWaveInRegion(currentsirs[0].element).sine.valueat(current_region_intersection.bounds[1].c[0]) < SineWaveInRegion(currentsirs[1].element).sine.valueat(current_region_intersection.bounds[1].c[0]);
+        if valid_beggining<>valid_ending then
+        	begin     							//change - valid region becomes non-valid, or vice-versa
+            intersectionwave:= SineWaveInRegion(currentsirs[0].element).sine.intersectWave(SineWaveInRegion(currentsirs[1].element).sine);
+            if current_region_intersection.inside(intersectionwave.getZeros()[0]) then
+            	intersection:= intersectionwave.getZeros()[0]
+            else
+            	intersection:= intersectionwave.getZeros()[1];
+            if current_valid_region=nil then          //in the beggining...
+            	if valid_beggining then
+                	begin
+                    current_valid_region:= TRegion.create(PointND.create(0), nil);
+                    valid_current_region:= true;
+                	end
+                else
+                	valid_current_region:=false;
+            if valid_current_region then
+            	begin
+                current_valid_region.bounds[1]:=intersection.clone();
+                result.add(current_valid_region);
+                end
+            else
+            	begin
+                current_valid_region:= Tregion.create( current_region_intersection.bounds[0].clone(), nil );
+                end;
+            valid_current_region:= not valid_current_region
+            end;
+    	current_region_intersection.bounds[0]:=current_region_intersection.bounds[1];
+        current_region_intersection.bounds[1]:=SinewaveInRegion(currentsirs[done_sir].element).region.bounds[1];
+        currentsirs[done_sir]:= sirs[done_sir].advance();
+        end;
+
+    if valid_current_region then
+            	begin
+                current_valid_region.bounds[1]:=intersection.clone();
+                result.add(current_valid_region);
+                end ;
+
+    result.rewind();                 //all that follows is debug
+    for i:=0 to result.length-1 do
+    	begin
+        current_region_intersection:= Tregion(result.advance().element);
+    	showmessage('from ' + floattostr(current_region_intersection.bounds[0].c[0]) + ' to ' +floattostr(current_region_intersection.bounds[1].c[0]));
+        end;
+    end;
+
 
 procedure process();
 var i,j, bound, iter:integer;
@@ -172,7 +243,7 @@ var i,j, bound, iter:integer;
         sir.region.bounds[1]:= PointND.create(2*pi);
 
         end;
-
+    validregions();
     end;
 
 end.
