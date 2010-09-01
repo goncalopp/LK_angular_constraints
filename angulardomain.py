e8589bda7c462b92cfeb478e86b7577a748da93e
goncalopp <goncalopp@will.(none)>
2010-09-01 23:08:29 +0100

    refactored angulardomain

--- angulardomain.py
+++ angulardomain.py
@@ -4,46 +4,53 @@ from region import Region
 pi2= 2*pi
 
 class AngularDomain:
-    '''subdivides the 0--2pi interval into subintervals (regions)'''
-    def __init__(self):
-        self.regions=[]
+	'''subdivides the 0--2pi interval into subintervals (regions)'''
+	def __init__(self):
+		self.regions=[]
 
-    def __getitem__(self, key):
-        return self.regions[key]
+	def __getitem__(self, key):
+		return self.regions[key]
 
-    def __setitem__(self, key, value):
-        self.regions[key]=value
+	def __setitem__(self, key, value):
+		self.regions[key]=value
 
-    def __iter__(self):
-        return self.regions.__iter__()
+	def __iter__(self):
+		return self.regions.__iter__()
 
-    def insertRegion(self, region):
-        self.regions.append(region)
+	def __len__(self):
+		return len(self.regions)
 
-    def __len__(self):
-        return len(self.regions)
+	def __repr__(self):
+		s=["-"]*20
+		for r in self.regions:
+			s[r[0][0]]="|"
+			s[r[1][0]]="|"
+		return "".join(s)
 
-    def doubleCut(self, other, doreverse=True):
-        '''takes self and another AngularDomain, and slices both of them
-        so that each region on one has an equivalent one on the other
-        (the same end points for the region)'''
-        otherindex=0
-        for r in self[:-1]:
-            while otherindex<len(other) and other[otherindex][1]<=r[1]:
-                otherindex+=1
-            if otherindex<len(other):
-                newregions=other[otherindex].cutOnPoint(r[1])
-                other.regions.pop(otherindex)
-                for newregion in newregions:
-                    other.regions.insert(otherindex, newregion)
-                    otherindex+=1
-                otherindex-=1
-        if doreverse:
+
+
+	def insertRegion(self, region):
+		self.regions.append(region)
+
+	def cutRegionOnPoint(self, regionindex, point):
+		newregions=self[regionindex].cutOnPoint(point)
+		self.regions.pop(regionindex)
+		for newregion in newregions:
+			self.regions.insert(regionindex, newregion)
+			regionindex+=1
+		regionindex-=1
+		return regionindex
+
+
+	def doubleCut(self, other, doreverse=True):
+		'''takes self and another AngularDomain, and slices both of them
+		so that each region on one has an equivalent one on the other
+		(the same end points for the region)'''
+		otherindex=0
+		for r in self[:-1]:
+			while otherindex<len(other) and other[otherindex][1]<=r[1]:
+				otherindex+=1
+			if otherindex<len(other):
+				otherindex= other.cutRegionOnPoint(otherindex, r[1])
+		if doreverse:
 			other.doubleCut(self, doreverse=False)
-
-    def __repr__(self):
-        s=["-"]*20
-        for r in self.regions:
-            s[r[0][0]]="|"
-            s[r[1][0]]="|"
-        return "".join(s)
