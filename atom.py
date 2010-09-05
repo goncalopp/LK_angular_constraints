a3bb92cd2f21badbf6b8a75b33d7000571b0004c
goncalopp <goncalopp@will.(none)>
2010-09-05 17:06:30 +0100

    BUG: on atomsine_calc.sineFromAtomDomain(): sine.y was wrong. added __repr__() to atom

--- atom.py
+++ atom.py
@@ -4,3 +4,6 @@ class Atom:
 	def __init__(self, position, bound1, bound2):
 		self.position= position
 		self.region= Region(bound1, bound2)
+	
+	def __repr__(self):
+		return '<Atom '+str(self.position)+str(self.region)+'>'
