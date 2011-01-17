02e31f759fadd74ae7f69a855f6aa392c5e1d889
goncalopp <goncalopp@will.(none)>
2011-01-17 22:44:23 +0000

    ccorrected atomsine_test to reflect changes in dc4abe08c53e9bcde56e4aadc5dec44fe7be24ba

--- unit_tests/atomsine_test.py
+++ unit_tests/atomsine_test.py
@@ -11,13 +11,13 @@ assert equal(s.p, atan(p1[2]/p1[1])+pi)   # "+pi" because atom.toSine() calculat
 assert equal(s.y, 0.87)
 
 
-s = AtomSine(a, 0, 1)
+s = AtomSine(a, 1, 0)
 #projecting on y axis. x coordinate is z, y coordinate is x
 assert equal(s.a, sqrt(1.5*1.5+1.87*1.87))
 assert equal(s.p, atan(p1[0]/p1[2])+pi)   
 assert equal(s.y, 0.5)
 
-s = AtomSine(a, 0, 2)
+s = AtomSine(a, 2, 0)
 #projecting on z axis. x coordinate is x, y coordinate is y
 assert equal(s.a, sqrt(1.5*1.5+8.4*8.4))
 assert equal(s.p, atan(p1[1]/p1[0])+pi)   
