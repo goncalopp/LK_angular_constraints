bd29252f58f68bd910eaf46cfc0e3645fab67ee7
goncalopp <goncalopp@pekkala.(none)>
2011-01-20 17:22:41 +0000

    AngularDomain: BUG: redid doubleCut(); added doublecutIterator(), intersect(), removeIndexes() added more tests to angulardomain_test

--- src/angulardomain.py
+++ src/angulardomain.py
@@ -21,10 +21,15 @@ class AngularDomain:
 		return len(self.regions)
 
 	def __repr__(self):
-		s=["-"]*20
+		s=["-"]*32
 		for r in self.regions:
-			s[int(r[0][0]*3)]="|"
-			s[int(r[1][0]*3)]="|"
+			a=int(r[0][0]*10)
+			b=int(r[1][0]*10)
+			s[a]="|"
+			s[b]="|"
+			for i in xrange(a+1,b):
+				s[i]=":"
+			
 		return "".join(s)
 
 	def lookup(self, point):
@@ -49,18 +54,91 @@ class AngularDomain:
 
 
 	def doubleCut(self, other, doreverse=True):
-		'''takes self and another AngularDomain, and slices both of them
-		so that each region on one has an equivalent one on the other
-		(the same end points for the region)'''
-		otherindex=0
-		for r in self[:-1]:
-			while otherindex<len(other) and other[otherindex][1]<=r[1]:
-				otherindex+=1
-			if otherindex<len(other):
-				otherindex= other.cutRegionOnPoint(otherindex, r[1])
-		if doreverse:
-			other.doubleCut(self, doreverse=False)
+		'''takes self and another AngularDomain, and slices both of them,
+		i.e.: cuts each region in the endpoints of the other AngularDomains
+		regions, i.e.: ater this operation, for each region R1 in one
+		AngularDomain, if there exists an Region R2 in the other so that R1
+		contains R2 or R2 contains R1, the endpoints of R1 and R2 are the
+		same.'''
+		i,j= 0,0
+		while i<len(self) and j<len(other):
+			p= Region.relativePosition(self[i],other[j])
+			if p==1:
+				i+=1
+			elif p==2:
+				j+=1
+			elif p==3:
+				self.cutRegionOnPoint(i, other[j][0])
+				i+=1
+				other.cutRegionOnPoint(j, self[i][1])
+				j+=1
+			elif p==4:
+				other.cutRegionOnPoint(j, self[i][0])
+				j+=1
+				self.cutRegionOnPoint(i, other[j][1])
+				i+=1
+			elif p==5:
+				i= self.cutRegionOnPoint(i, other[j][0])
+				i= self.cutRegionOnPoint(i, other[j][1])
+				j+=1
+			elif p==6:
+				j= other.cutRegionOnPoint(j, self[i][0])
+				j= other.cutRegionOnPoint(j, self[i][1])
+				i+=1
+		#if doreverse:
+		#	other.doubleCut(self, doreverse=False)
 
+	def doublecutIterator(self,other):
+		'''iterates over two doublecut AngularDomain, giving tuples of
+		either (r1,r2), (r1,None) or (None, r2), r1 belonging to self, r2
+		belonging to the other AngularDomain'''
+		i,j= 0,0
+		while i<len(self) or j<len(other):
+			if i>=len(self) or (self[i][0]>other[j][0] and self[i][1]>other[j][1]):
+				yield (None, other[j])
+				j+=1
+			elif j>=len(other) or self[i][0]<other[j][0]:
+				yield (self[i], None)
+				i+=1
+			elif self[i][0]==other[j][0]:
+				if self[i][1]!=other[j]:
+					raise Exception("The given AngularDomains are not doublecut")
+				yield (self[i], other[j])
+				i+=1
+				j+=1
+			else:
+				raise Exception("The given AngularDomains are not doublecut")
+		
+
+	def intersect(self, other):
+		'''takes self and other AngularDomain and removes from both the
+		region "pieces" that don't belong to both, i.e.: after this
+		operation, both AngularDomains become their intersection'''
+		self.doubleCut(other)
+		self_removals, other_removals= [],[]
+		i,j= 0,0
+		for a,b in self.doublecutIterator(other):
+			if a==None:
+				other_removals.append(j)
+				j+=1
+			elif b==None:
+				self_removals.append(i)
+				i+=1
+			else:
+				i+=1
+				j+=1
+
+		print self_removals
+		print other_removals
+		self.removeIndexes(self_removals)
+		other.removeIndexes(other_removals)
+
+	def removeIndexes(self, index_list):
+		i=0
+		for index in index_list:
+			self.regions.pop(index-i)
+			i+=1
+		
 	def mergeAdjacentRegions(self):
 		'''joins adjacent regions'''
 		i=0
--- unit_tests/angulardomain_test.py
+++ unit_tests/angulardomain_test.py
@@ -1,57 +1,128 @@
+l= [PointND([i/10.0]) for i in range(32)]
+
 a1= AngularDomain()
 a2= AngularDomain()
+a1.insertRegion( Region(l[ 0], l[10]) )
+a1.insertRegion( Region(l[10], l[20]) )
+a1.insertRegion( Region(l[20], l[30]) )
+a2.insertRegion( Region(l[ 0], l[15]) )
+a2.insertRegion( Region(l[15], l[25]) )
+a2.insertRegion( Region(l[25], l[30]) )
 
-r1= Region(PointND([0]), PointND([1]))
-r2= Region(PointND([1]), PointND([2]))
-r3= Region(PointND([2]), PointND([3]))
+assert a1.lookup(l[23])==2
+assert a1.lookup(l[20])==1		#returns *first* region which has "2"
 
-r4= Region(PointND([0]), PointND([1.5]))
-r5= Region(PointND([1.5]), PointND([2.5]))
-r6= Region(PointND([2.5]), PointND([3]))
-
-
-a1.insertRegion(r1)
-a1.insertRegion(r3)
-a1.insertRegion(r2)
-assert a1[0]==r1
-assert a1[1]==r3
-assert a1[2]==r2
-
-a1= AngularDomain()
-a1.insertRegion(r1)
-a1.insertRegion(r2)
-a1.insertRegion(r3)
-
-a2.insertRegion(r4)
-a2.insertRegion(r5)
-a2.insertRegion(r6)
-
-assert a1.lookup(PointND([2.3]))==2
-assert a1.lookup(PointND([2]))==1		#returns *first* region which has "2"
-
-a1.cutRegionOnPoint(2, PointND([2.3]))	#REMEMBER THIS FOR LATER TESTS
-assert a1[2][0][0]==2
-assert a1[2][1][0]==2.3
-assert a1[3][0][0]==2.3
-assert a1[3][1][0]==3
+a1.cutRegionOnPoint(2, l[23])	#REMEMBER THIS FOR LATER TESTS
+assert a1[2][0]==l[20]
+assert a1[2][1]==l[23]
+assert a1[3][0]==l[23]
+assert a1[3][1]==l[30]
 
 a1.doubleCut(a2)
-for i in range(6):
-	assert a1[i]==a2[i]
 
-assert a1[0]== Region(PointND([0]),PointND([1]))
-assert a1[1]== Region(PointND([1]),PointND([1.5]))
-assert a1[2]== Region(PointND([1.5]),PointND([2]))
-assert a1[3]== Region(PointND([2]),PointND([2.3]))
-assert a1[4]== Region(PointND([2.3]),PointND([2.5]))
-assert a1[5]== Region(PointND([2.5]),PointND([3]))
+for i in range(6):
+	assert a1[i][0]==a2[i][0]
+	assert a1[i][1]==a2[i][1]
+assert a1[0]== Region(l[ 0], l[10])
+assert a1[1]== Region(l[10], l[15])
+assert a1[2]== Region(l[15], l[20])
+assert a1[3]== Region(l[20], l[23])
+assert a1[4]== Region(l[23], l[25])
+assert a1[5]== Region(l[25], l[30])
 
 a1.mergeAdjacentRegions()
 assert len(a1)==1
-assert a1[0]== Region(PointND([0]),PointND([3]))
-
-a2[2][0]=PointND([1.6])
+assert a1[0]== Region(l[ 0],l[30])
+a2[2][0]= l[16]
 a2.mergeAdjacentRegions()
 assert len(a2)==2
-assert a2[0]== Region(PointND([0]),PointND([1.5]))
-assert a2[1]== Region(PointND([1.6]),PointND([3]))
+assert a2[0]== Region(l[ 0], l[15])
+assert a2[1]== Region(l[16], l[30])
+
+
+#---------------------------------------------
+
+a1= AngularDomain()
+a2= AngularDomain()
+a1.insertRegion( Region(l[ 0], l[ 5]) )
+a1.insertRegion( Region(l[10], l[20]) )
+a1.insertRegion( Region(l[25], l[30]) )
+a2.insertRegion( Region(l[00], l[15]) )
+a2.insertRegion( Region(l[20], l[30]) )
+
+a1.doubleCut(a2)
+assert len(a1)==4
+assert len(a2)==5
+assert a1[0]== Region(l[ 0], l[ 5])
+assert a1[1]== Region(l[10], l[15])
+assert a1[2]== Region(l[15], l[20])
+assert a1[3]== Region(l[25], l[30])
+assert a2[0]== Region(l[ 0], l[ 5])
+assert a2[1]== Region(l[ 5], l[10])
+assert a2[2]== Region(l[10], l[15])
+assert a2[3]== Region(l[20], l[25])
+assert a2[4]== Region(l[25], l[30])
+
+a1.intersect(a2)
+assert len(a1)==3
+assert len(a2)==3
+assert a1[0]== a2[0]== Region(l[ 0], l[ 5])
+assert a1[1]== a2[1]== Region(l[10], l[15])
+assert a1[2]== a2[2]== Region(l[25], l[30])
+
+#---------------------------------------------
+
+a1= AngularDomain()
+a2= AngularDomain()
+a1.insertRegion( Region(l[ 0], l[ 2]) )
+a1.insertRegion( Region(l[ 3], l[ 5]) )
+a1.insertRegion( Region(l[ 7], l[10]) )
+a1.insertRegion( Region(l[11], l[15]) )
+a1.insertRegion( Region(l[17], l[19]) )
+a1.insertRegion( Region(l[21], l[24]) )
+a1.insertRegion( Region(l[26], l[28]) )
+a2.insertRegion( Region(l[ 0], l[ 2]) )
+a2.insertRegion( Region(l[ 3], l[ 6]) )
+a2.insertRegion( Region(l[ 7], l[ 9]) )
+a2.insertRegion( Region(l[12], l[14]) )
+a2.insertRegion( Region(l[16], l[20]) )
+a2.insertRegion( Region(l[22], l[24]) )
+a2.insertRegion( Region(l[25], l[28]) )
+
+
+a1.doubleCut(a2)
+assert len(a1)==11
+assert len(a2)==11
+assert a1[0]== Region(l[ 0], l[ 2])
+assert a1[1]== Region(l[ 3], l[ 5])
+assert a1[2]== Region(l[ 7], l[ 9])
+assert a1[3]== Region(l[ 9], l[10])
+assert a1[4]== Region(l[11], l[12])
+assert a1[5]== Region(l[12], l[14])
+assert a1[6]== Region(l[14], l[15])
+assert a1[7]== Region(l[17], l[19])
+assert a1[8]== Region(l[21], l[22])
+assert a1[9]== Region(l[22], l[24])
+assert a1[10]== Region(l[26], l[28])
+assert a2[0]== Region(l[ 0], l[ 2])
+assert a2[1]== Region(l[ 3], l[ 5])
+assert a2[2]== Region(l[ 5], l[ 6])
+assert a2[3]== Region(l[ 7], l[ 9])
+assert a2[4]== Region(l[12], l[14])
+assert a2[5]== Region(l[16], l[17])
+assert a2[6]== Region(l[17], l[19])
+assert a2[7]== Region(l[19], l[20])
+assert a2[8]== Region(l[22], l[24])
+assert a2[9]== Region(l[25], l[26])
+assert a2[10]== Region(l[26], l[28])
+
+a1.intersect(a2)
+assert len(a1)==7
+assert len(a2)==7
+assert a1[0]== a2[0]== Region(l[ 0], l[ 2])
+assert a1[1]== a2[1]== Region(l[ 3], l[ 5])
+assert a1[2]== a2[2]== Region(l[ 7], l[ 9])
+assert a1[3]== a2[3]== Region(l[12], l[14])
+assert a1[4]== a2[4]== Region(l[17], l[19])
+assert a1[5]== a2[5]== Region(l[22], l[24])
+assert a1[6]== a2[6]== Region(l[26], l[28])
