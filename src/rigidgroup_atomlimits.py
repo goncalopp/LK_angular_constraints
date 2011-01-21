e1354fb99958cf65f00336ec9d043046380e009d
goncalopp <goncalopp@gmail.com>
2011-01-21 21:06:10 +0000

    completed rigidgroup_atomlimits. results seem incorrect

--- src/rigidgroup_atomlimits.py
+++ src/rigidgroup_atomlimits.py
@@ -1,35 +1,40 @@
 from region import Region
 from rigidgroup import RigidGroup
+from sine import Sine
+from pointnd import PointND
 from rigidgroup_centerlimits import calculateCenterLimits
 
-def calculateAtomLimits(rigidgroup):
-	'''given a rigidgroup (which is in a certain orientation, returns
+def calculateAtomLimits(rigidgroup, rotation_axis):
+	'''given a rigidgroup (which is in a certain orientation), returns
 	an dictionary whose keys are Atoms and values are Regions, which
 	represent the new atom limits'''
-	tmp= \
-		[calculateCenterLimits(rigidgroup, coordinate)
-		for coordinate in (0,1,2)]	#x,y,z
-	print "tmp",tmp
-	raw_input()
-	tmp2= zip(tmp)	#tmp2 becomes two lists, one of sines, other of angulardomains
-	sines= tmp2[0]
-	centerdomains= tmp2[1]
+	#x and y coordinates, for rotating around rotation_axis
+	x, y= [(rotation_axis + 1) % 3, (rotation_axis + 2) % 3]
+	tmp= [calculateCenterLimits(rigidgroup, rotation_axis, c) for c in (x,y)]
+	tmp2= zip(*tmp)	#tmp2 becomes two lists, one of sines, other of angulardomains
+	sines= tmp2[0]	#sines[0] is the sines for x-coord, sines[1] for y-coord
+	center_domains= tmp2[1]	#...and similarly for the domains
 	
-	for bound in (0,1):
-		for coord in (0,1,2):
-			centerdomains[coord%3][bound].intersect(centerdomains[coord%3][bound], bothways=True)
+	for bound in (0,1):	#intersect the valid domains of the two coordinates
+		center_domains[0][bound].intersect(center_domains[1][bound], bothways=True)
 	
 	atomdomains= [calculateAtomLimits_coord(sines[c], center_domains[c], c) \
-	for c in (0,1,2)]
+	for c in (0,1)]
 	
 	region_dict= {}	#stores final atom regions
+	minmaxes= [calculateAtomLimits_coord(sines[c], center_domains[c], rotation_axis) for c in (0,1)]	
 	for atom in rigidgroup.atoms:
-		minmaxes= [centerdomains[c][atom] for c in (0,1,2)]
-		lower_limit= PointND([minmaxes[c][0] for c in (0,1,2)])
-		upper_limit= PointND([minmaxes[c][1] for c in (0,1,2)])
+		atom_lower_limits= [minmaxes[c][atom][0] for c in (0,1)]
+		atom_upper_limits= [minmaxes[c][atom][1] for c in (0,1)]
+		print atom_lower_limits
+		lower_limit= PointND([float("-inf")]*3)
+		upper_limit= PointND([float("inf")]*3)
+		lower_limit[x]= atom_lower_limits[0]
+		lower_limit[y]= atom_lower_limits[1]
+		upper_limit[x]= atom_upper_limits[0]
+		upper_limit[y]= atom_upper_limits[1]
+		
 		region_dict[atom]= Region(lower_limit, upper_limit)
-		print atom, lower_limit, upper_limit
-	
 	return region_dict
 
 def calculateAtomLimits_coord(sines, validdomains, coordinate):
